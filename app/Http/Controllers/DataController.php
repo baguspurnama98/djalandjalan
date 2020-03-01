930a0c80110eec925a806b20ff900a7d05711ae2
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-03-01 12:35:55 +0700

    Fix Data Controller Not Assume git update-index --no-assume-unchanged app/Http/Controllers/DataController.php git update-index --really-refresh

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -11,4 +11,9 @@ class DataController extends Controller
         $trip = Trip::with(['user','place'])->take(4)->get();
         return view('landing', ['trip' => $trip]);
     }
+
+    public function partner(){
+        $trip = Trip::with(['user','place'])->get();
+        return view('landing', ['trip' => $trip]);
+    }
 }
