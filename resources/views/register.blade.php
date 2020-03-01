e7710a1357393c529c28f51aa4abd2b7583890aa
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-03-01 14:57:24 +0700

    Update Register Create Function

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,13 +4,14 @@
 <div class="limiter" style="background-color: #2ED1A2;">
     <div class="container-login100">
         <div class="wrap-login100 p-l-50 p-r-50 p-t-30 p-b-30" style="width: 600px;">
-            <form class="login100-form validate-form">
+            <form class="login100-form validate-form" action="{{ url('register') }}" method="POST">
+                @csrf
                 <span class="login100-form-title p-b-25">
                     <a href="{{ url('/') }}"><img src="{{ URL::asset('assets/assets/img/djalandjalanlogo.png') }}" style="width: 50%;"></a>
                 </span>
 
                 <div class="wrap-input100 m-b-10">
-                    <input class="input100" type="text" name="namaLengkap" placeholder="Nama Lengkap">
+                    <input class="input100" type="text" value="{{ old('full_name') }}" name="full_name" placeholder="Nama Lengkap">
                     <span class="focus-input100"></span>
                     <span class="symbol-input100">
                         <span class="lnr lnr-user"></span>
@@ -18,7 +19,7 @@
                 </div>
 
                 <div class="wrap-input100 validate-input m-b-10" data-validate="Valid email is required: ex@abc.xyz">
-                    <input class="input100" type="text" name="email" placeholder="Email">
+                    <input class="input100" type="text" value="{{ old('email') }}" name="email" placeholder="Email">
                     <span class="focus-input100"></span>
                     <span class="symbol-input100">
                         <span class="lnr lnr-envelope"></span>
@@ -26,7 +27,7 @@
                 </div>
 
                 <div class="wrap-input100 validate-input m-b-15" data-validate="Handphone is required">
-                    <input class="input100" type="text" name="noHp" placeholder="No. Handphone">
+                    <input class="input100" type="text" value="{{ old('phone') }}" name="phone" placeholder="No. Handphone">
                     <span class="focus-input100"></span>
                     <span class="symbol-input100">
                         <span class="lnr lnr-phone-handset"></span>
@@ -34,7 +35,7 @@
                 </div>
 
                 <div class="wrap-input100 validate-input m-b-15" data-validate="Tanggal Lahir is required">
-                    <input class="input100" type="text" name="tglLahir" placeholder="Tanggal Lahir">
+                    <input class="input100" type="text" value="{{ old('birth_day') }}" name="birth_day" placeholder="Tanggal Lahir">
                     <span class="focus-input100"></span>
                     <span class="symbol-input100">
                         <span class="lnr lnr-calendar-full"></span>
@@ -43,13 +44,12 @@
 
                 <div class="wrap-input100 validate-input m-b-15" data-validate="Handphone is required">
                     <span>Jenis Kelamin :</span>
-                    <input type="radio" value="Laki" name="gender"> Laki-Laki &emsp;
-                    <input type="radio" value="Perempuan" name="gender"> Perempuan
-
+                    <input type="radio" value="1" name="gender"> Laki-Laki &emsp;
+                    <input type="radio" value="0" name="gender"> Perempuan
                 </div>
 
                 <div class="wrap-input100 validate-input m-b-15" data-validate="Password is required">
-                    <input class="input100" type="password" name="pass" placeholder="Password">
+                    <input class="input100" type="password" name="password" placeholder="Password">
                     <span class="focus-input100"></span>
                     <span class="symbol-input100">
                         <span class="lnr lnr-lock"></span>
@@ -57,7 +57,7 @@
                 </div>
 
                 <div class="wrap-input100 validate-input m-b-25" data-validate="!!!">
-                    <input class="input100" type="password" name="confirm_pass" placeholder="Ketik Ulang Password">
+                    <input class="input100" type="password" name="password_confirmation" placeholder="Ketik Ulang Password">
                     <span class="focus-input100"></span>
                     <span class="symbol-input100">
                         <span class="lnr lnr-checkmark-circle"></span>
@@ -65,14 +65,14 @@
                 </div>
 
                 <div class="contact100-form-checkbox m-l-4">
-                    <input class="input-checkbox100" id="ckb1" type="checkbox" name="aggrePolicy">
+                    <input class="input-checkbox100" id="ckb1" type="checkbox" name="agree">
                     <label class="label-checkbox100" for="ckb1">
                         <span style="font-size: 12px;">Saya menyetujui Aturan Pengguna dan Kebijakan Privasi djalandjalan.com</span>
                     </label>
                 </div>
 
                 <div class="container-login100-form-btn p-t-5">
-                    <button class="login100-form-btn">
+                    <button type="submit" class="login100-form-btn">
                         Daftar
                     </button>
                 </div>
